7846608ecb7b20d79b388a38369129b0221ad554
SergioSpagnol <sergiospagnol.ss@gmail.com>
2023-03-17 12:25:43 -0300

    Initial commit

---
+++ Portifolio.API/Migrations/20230316135847_teste4.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portifolio.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class teste4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "Contatos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Contatos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Menssage",
+                table: "Contatos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Contatos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "Contatos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Contatos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Menssage",
+                table: "Contatos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Contatos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Portifolio.API/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using Portifolio.API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSqlServer<PortifolioContext>(builder.Configuration.GetConnectionString("ServerConnection"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();   
+builder.Services.AddCors();
+
+var app = builder.Build();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseCors(p => p
+    .AllowAnyHeader()
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+);
+
+
+app.MapPost("/Contacts", async (PortifolioContext context, Contact contact) =>
+{
+    contact.Date = DateTime.Now;
+
+    await context.Contatos.AddAsync(contact);
+    await context.SaveChangesAsync();
+
+    return Results.Ok(contact);
+})
+.WithOpenApi();
+
+app.MapGet("/Contacts", async (PortifolioContext context) =>
+{
+    var contacts = await context.Contatos.ToListAsync();
+
+    return Results.Ok(contacts);
+})
+.WithOpenApi();
+
+app.Run();
+
+public class Contact 
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Subject { get; set; }
+    public string Menssage { get; set; }
+    public DateTime Date { get; set; }
+
+}
